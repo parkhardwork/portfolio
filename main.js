c6569465ea7b1f689482314fb4fd7ca479c8f2df
parkhardwork <parkhardwork@gmail.com>
2023-02-10 06:07:14 +0900

    JS: remove active from nav

--- main.js
+++ main.js
@@ -74,6 +74,12 @@ workBtnContainer.addEventListener('click', (e) => {
         return;
     }
 
+    // Remove selection from the preivous item and select the new one
+    const active = document.querySelector('.category__btn.selected')
+    active.classList.remove('selected');
+    const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+    e.target.classList.add('selected');
+
     projectContainer.classList.add('anim-out');
 
     projects.forEach((project) => {
