0063cb24dc5b2b16e7dfdb15aafc117131e1e74f
parkhardwork <parkhardwork@gmail.com>
2023-02-13 04:51:12 +0900

    JS: Navbar menu toggle + project UI

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ document.addEventListener('scroll', ()=> {
     }
 })
 
-
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
@@ -23,12 +22,15 @@ navbarMenu.addEventListener('click', (event) => {
         return;
     }
 
-    // console.log(event.target.dataset.link);
-    // const scrollTo = document.querySelector(link);
-    // scrollTo.scrollIntoView({behavior: "smooth"});
-
+    navbarMenu.classList.remove('open');
     scrollIntoViews(link);
 })
+
+// Navbar toggle button for small screen
+const navbarToggleBn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+});
 
 // Handle click on "contact me" button on home
 
